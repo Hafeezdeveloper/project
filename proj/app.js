776c8e63ed1e2b6f60a1f6e9f2ba6192912935c1
Hafeezdeveloper <Hafeez.hussain2003@gmail.com>
2022-11-26 12:59:19 +0500

    pro

---
+++ proj/app.js
@@ -0,0 +1,11 @@
+function abc(){
+    window.open ("https://evampsaanga.com/wp-content/uploads/2022/01/ISO27001-Certificate-January-2022.pdf")
+}
+
+function getaQo(){
+    window.location.href = "getQ.html"
+}
+
+function get_hom(){
+    window.location.href = "index.html"
+}
